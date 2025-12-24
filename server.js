17d4267534cc55290c59c13c80baf5359ad2b58f
jcnnylee <jennyylee0315@gmail.com>
2025-12-24 02:24:51 -0500

    JWT Secret Key

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
+
+// Authentication imports
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 // Imported these modules because I kept getting an error with Prisma when trying to run the backend server
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const { PrismaClient } = require('./generated/prisma')
 const { PrismaPg } = require('@prisma/adapter-pg')
-
-import cors from 'cors'
 
 const app = express()
 
@@ -50,13 +52,40 @@ app.post("/register", async (req, res) => {
             password: hashedPassword,
         }
     }) 
-    res.status(201).json(user)
+    res.status(201).json(user) //checks for the error and simplifies error
     } catch (error) {
         res.status(500).json({ 
             error: "There is an error with the server!", 
             details: String(error) 
         })
     }
+})
+
+app.post("/login", async (req, res) => {
+    
+        const { email, password } = req.body
+        // Checks if the user exists in the database
+        const user = await prisma.user.findUnique({ 
+            where: { email: email } 
+        })
+
+        if (!user) {
+            return res.status(400).json({ 
+                error: "Unauthorized! User not found!!!" 
+            })
+        }
+
+        // Compares the password with the hashed password 
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ 
+                error: "Invalid password!"
+            })
+        }
+
+        // Generates a JWT token for user
+        const token = jwt.sign({userId: user.id}, 'SECRET_KEY__1234', { expiresIn: '1hr'})
+        res.json({token})
 })
 
 app.get('/', (req, res) => {
